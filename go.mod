b18ec210edebf56b118e592efbe3de1ee72bb8e8
greg <gregorycusack@gmail.com>
2020-04-24 17:13:41 -0700

    read in deploy json and deploy pods

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/gregcusack/ec_deployer
 
 go 1.14
 
-require github.com/Maziyar-Na/EC-Agent v0.0.0-20200423213827-480c269ad070
+require (
+	github.com/Maziyar-Na/EC-Agent v0.0.0-20200423213827-480c269ad070
+	k8s.io/api v0.0.0-20200421221621-2ca06ba83edf
+	k8s.io/apimachinery v0.0.0-20200423195709-147f4cafe1e5
+	k8s.io/client-go v0.0.0-20200424080147-0ad81dc3c85d
+)
--- main.go
+++ main.go
@@ -1,33 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/gregcusack/ec_deployer/structs"
 	"io/ioutil"
+	apiv1 "k8s.io/api/core/v1"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
-	"github.com/gregcusack/ec_deployer/structs"
-	"github.com/Maziyar-Na/EC-Agent/msg"
 )
 
 
-//type DeploymentDefinition struct {
-//	DCDefs []struct {
-//		Name  string `json:"name"`
-//		Specs struct {
-//			Mem   int `json:"mem"`
-//			CPU   int `json:"cpu"`
-//			Ports int `json:"ports"`
-//			Net   int `json:"net"`
-//		} `json:"specs"`
-//		GcmIP    string   `json:"gcmIP"`
-//		AgentIPs []string `json:"agentIPs"`
-//		Images   []string `json:"images"`
-//		PodNames []string `json:"pod-names"`
-//		YamlPath []string `json:"yaml_path"`
-//	} `json:"DC-def"`
-//}
 
 func main() {
 
@@ -48,17 +41,74 @@ func main() {
 	//var dcDefs DcDefs
 	var dcDefs structs.DeploymentDefinition
 
-	//json.Unmarshal(byteVal, &dcDefs)
 	json.Unmarshal(byteVal, &dcDefs)
 
-	for i := 0; i < len(dcDefs.DCDefs); i++ {
-		fmt.Println("App Name: " + dcDefs.DCDefs[i].Name)
-		fmt.Println("Mem Specs: " + strconv.Itoa(dcDefs.DCDefs[i].Specs.Mem))
-		fmt.Println("Cpu Specs: " + strconv.Itoa(dcDefs.DCDefs[i].Specs.CPU))
-		fmt.Println("Pod Names: " + dcDefs.DCDefs[i].PodNames[i])
+	clientset := configK8()
+	podClient := clientset.CoreV1().Pods(apiv1.NamespaceDefault)
+
+	//Generate Pod Defs and Deploy Pods
+	for i := 0; i < len(dcDefs.DCDefs[0].PodNames); i++ {
+		pod := createPodDefinition(&dcDefs.DCDefs[0].PodNames[i], &dcDefs.DCDefs[0].Images[i], &dcDefs)
+		result, err := podClient.Create(context.TODO(), pod, metav1.CreateOptions{})
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Pod created successfully: " + result.GetObjectMeta().GetName())
 	}
 
+}
+func configK8() *kubernetes.Clientset {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
 
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
 
+	return clientset
+}
 
+func createPodDefinition(podName *string, appImage *string, depDef *structs.DeploymentDefinition) *core.Pod {
+	specs := depDef.DCDefs[0].Specs
+	return &core.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      *podName,
+			Namespace: "default",
+			Labels: map[string]string{
+				"name": *podName,
+			},
+		},
+		Spec: core.PodSpec{
+			Containers: []core.Container{
+				{
+					Name:            *podName,
+					Image:           *appImage,
+					ImagePullPolicy: core.PullIfNotPresent,
+					Resources: apiv1.ResourceRequirements {
+						Requests: apiv1.ResourceList {
+							"memory": resource.MustParse(strconv.Itoa(specs.Mem) + "Mi"),
+							"cpu": resource.MustParse(strconv.Itoa(specs.CPU) + "m"),
+						},
+						Limits: apiv1.ResourceList {
+							"memory": resource.MustParse(strconv.Itoa(specs.Mem) + "Mi"),
+							"cpu": resource.MustParse(strconv.Itoa(specs.CPU) + "m"),
+						},
+
+					},
+
+				},
+			},
+		},
+	}
 }
