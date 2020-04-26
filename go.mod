ee08853d27c070e31590a89603564f17e7c681ad
greg <gregorycusack@gmail.com>
2020-04-26 14:34:47 -0700

    protobuf write sends twice or agent reads twice. not sure why

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/gregcusack/ec_deployer
 go 1.14
 
 require (
-	github.com/Maziyar-Na/EC-Agent v0.0.0-20200423213827-480c269ad070
+	github.com/Maziyar-Na/EC-Agent v0.0.0-20200426204712-8be1279858ac
 	k8s.io/api v0.0.0-20200421221621-2ca06ba83edf
 	k8s.io/apimachinery v0.0.0-20200423195709-147f4cafe1e5
 	k8s.io/client-go v0.0.0-20200424080147-0ad81dc3c85d
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/Maziyar-Na/EC-Agent/msg"
+	"github.com/golang/protobuf/proto"
 	"github.com/gregcusack/ec_deployer/structs"
 	"io/ioutil"
 	apiv1 "k8s.io/api/core/v1"
@@ -15,12 +17,15 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"log"
+	"net"
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 )
 
-
+const AGENT_PORT = ":4445"
+const BUFFSIZE = 2048
 
 func main() {
 
@@ -44,20 +49,92 @@ func main() {
 	json.Unmarshal(byteVal, &dcDefs)
 
 	clientset := configK8()
-	podClient := clientset.CoreV1().Pods(apiv1.NamespaceDefault)
+	//podClient := clientset.CoreV1().Pods(apiv1.NamespaceDefault)
 
 	//Generate Pod Defs and Deploy Pods
 	for i := 0; i < len(dcDefs.DCDefs[0].PodNames); i++ {
 		pod := createPodDefinition(&dcDefs.DCDefs[0].PodNames[i], &dcDefs.DCDefs[0].Images[i], &dcDefs)
-		result, err := podClient.Create(context.TODO(), pod, metav1.CreateOptions{})
+		//result, err := podClient.Create(context.TODO(), pod, metav1.CreateOptions{})
+		//
+		//if err != nil {
+		//	panic(err)
+		//}
+		//fmt.Println("Pod created successfully: " + result.GetObjectMeta().GetName())
 
-		if err != nil {
-			panic(err)
+		//Get Nodes from pod names
+		podObj, _ := clientset.CoreV1().Pods(apiv1.NamespaceDefault).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+
+		nodeObj,_ := clientset.CoreV1().Nodes().Get(context.TODO(), podObj.Spec.NodeName, metav1.GetOptions{})
+		nodeIP := nodeObj.Status.Addresses[0].Address
+		fmt.Println("Node Name: " + podObj.Spec.NodeName + ", Node ip: " + nodeIP)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		for {
+			//out := podObj.Status.Phase
+			pObj, _ := clientset.CoreV1().Pods(apiv1.NamespaceDefault).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+			out := pObj.Status.Phase
+			//fmt.Println(out)
+			if string(out) == "Running" || ctx.Err() != nil {
+				fmt.Println("Pod Status: " + string(out))
+				break
+			}
 		}
-		fmt.Println("Pod created successfully: " + result.GetObjectMeta().GetName())
+
+		//TODO: Get agent from node IP
+		//Should be resolved when we add agent to kubelet
+		//We know IP and we know port (4445), so just send a request to it
+		//fmt.Println(podObj)
+		getPodInfoFromAgent(nodeIP, podObj.Name)
+
+		//TODO: get cgroup ID from pod
+
+		break
+
 	}
 
+
+
+
+	//get node ips from node names
+
 }
+
+
+func getPodInfoFromAgent(agentIP, podName string) {
+	txMsg := &msg_struct.ECMessage{
+		ClientIp: proto.String("127.0.0.1"),
+		CgroupId: proto.Int32(0),
+		ReqType: proto.Int32(6),
+		PayloadString: proto.String(podName),
+		//RsrcAmnt: proto.Uint64(9),
+		//Quota: proto.Uint64(8),
+	}
+	fmt.Println(txMsg)
+	txMsgMarshal, err := proto.Marshal(txMsg)
+	checkError(err)
+	fmt.Println(txMsgMarshal)
+
+	fmt.Println(agentIP + AGENT_PORT)
+	conn, _ := net.Dial("tcp", agentIP + AGENT_PORT)
+	defer conn.Close()
+	n, err := conn.Write(txMsgMarshal)
+	checkError(err)
+	fmt.Println("Sent " + strconv.Itoa(n) + " bytes")
+
+	//buff := make([]byte, BUFFSIZE)
+	rxMsg := &msg_struct.ECMessage{}
+	err = proto.Unmarshal(txMsgMarshal,rxMsg)
+	checkError(err)
+	fmt.Println(rxMsg)
+
+
+
+
+
+
+}
+
 func configK8() *kubernetes.Clientset {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
@@ -110,5 +187,11 @@ func createPodDefinition(podName *string, appImage *string, depDef *structs.Depl
 				},
 			},
 		},
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Println("[ERROR]: " + err.Error())
 	}
 }
